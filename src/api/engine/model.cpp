fae3769520a36894b504dc62371cb78c76eb2ec7
Finian <finb7729@gmail.com>
2019-05-25 21:04:12 -0500

    add enet

---
+++ src/api/engine/model.cpp
@@ -0,0 +1,5 @@
+//
+// Created by finian on 5/25/19.
+//
+
+#include "model.h"
---
+++ src/api/engine/model.h
@@ -0,0 +1,14 @@
+//
+// Created by finian on 5/25/19.
+//
+
+#ifndef INC_3DPLATFORMER_MODEL_H
+#define INC_3DPLATFORMER_MODEL_H
+
+
+class model {
+
+};
+
+
+#endif //INC_3DPLATFORMER_MODEL_H
